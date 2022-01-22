3ac20132ab6318aaffc199e4fb92a20ca65f7bd4
Evgeniy <ke5495251095@gmail.com>
2022-01-22 20:42:01 +0300

    первые потуги тугосери

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QDir>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +14,33 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_InputButton_released()
+{
+    QString fileName = QFileDialog::getOpenFileName(
+                this,
+                tr("Open File"),
+                "/home",
+                tr("Images (*.png *.xpm *.jpg *.dds *.tga)")
+                );
+    ui->lineInput->setText(fileName);
+
+    //QMessageBox::warning(this, "Ошибка!","Не указанно имя флага! ");
+}
+
+
+void MainWindow::on_OutputButton_released()
+{
+    QString dirName = QFileDialog::getExistingDirectory(
+                this,
+                tr("Open Directory"),
+                "/home",
+                QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks
+                );
+    ui->lineOutput->setText(dirName);
+
+    //QMessageBox::warning(this, "Ошибка!","Не указанно имя флага! ");
 }
 
